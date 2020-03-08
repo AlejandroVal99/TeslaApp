ef8cd0f1a8569ffe0e3aff0810c71790a9388020
Alejandro Valverde <joalevalverde@hotmail.com>
2020-03-08 15:57:12 -0500

    Imagenes y tipografias

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -15,9 +15,7 @@ public class MainView extends PApplet {
 	private DeliveryInfoView deliveryView;
 	private PaymentView paymentView;
 	private ThanksView thanksView;
-	
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PApplet.main("view.MainView");
@@ -28,12 +26,50 @@ public class MainView extends PApplet {
 	}
 
 	public void setup() {
-		
-		
 
 	}
-	
+
 	public void draw() {
+
+		switch (screen) {
+		
+		case 1://splashScreen
+			
+			break;
+		case 2://Log in screen
+		
+			break;
+		case 3://sign in o register screen
+			
+			break;
+		case 4://Select Model screen
+			
+			break;
+		case 5://Info Model Screen
+			
+			break;
+		case 6://Compare menu screen 
+			
+			break;
+		case 7://Compare Screen
+			
+			break;
+		case 8://Info delivery Screen
+		
+			break;
+		case 9://historico Screen
+			break;
+		case 10://Thanks Screen
+			break;
+		case 11://Payment Screen
+			break;
+
+		}
+
+	}
+
+	public void mousePressed(){
 		
 	}
+
 }
